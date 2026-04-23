a9095332efe7feb9b0ae10d67896ea40681f57fd
misha <misha@MIKHAIL>
2026-04-24 01:55:44 +0700

    Реализован сервис парсинга строки

---
+++ OrderAnalytics.Application/Abstractions/IOrderImportParser.cs
@@ -0,0 +1,22 @@
+﻿namespace OrderAnalytics.Application.Abstractions
+{
+	using OrderAnalytics.Application.DTO;
+
+	/// <summary>
+	/// Интерфейс для сервиса парсинга строки в заказ.
+	/// </summary>
+	public interface IOrderImportParser
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Пытается распарсить строку CSV в заказ.
+		/// </summary>
+		/// <param name="line">Строка CSV.</param>
+		/// <param name="lineNumber">Номер строки.</param>
+		/// <returns>Результат парсинга строки.</returns>
+		OrderImportParseResult TryParseLine(string line, int lineNumber);
+
+		#endregion Public Methods
+	}
+}
