3af034218e2980897ad525ddb7ce97971e05fb38
misha <misha@MIKHAIL>
2026-04-24 01:56:23 +0700

    Реализованы DTO для результатов импорта

---
+++ OrderAnalytics.Application/DTO/CsvImportResult.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderAnalytics.Application.DTO
+{
+	using OrderAnalytics.Domain.Entities;
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Результат импорта CSV файла.
+	/// </summary>
+	/// <param name="Orders"></param>
+	/// <param name="Errors"></param>
+	public record CsvImportResult(List<Order> Orders, List<ValidationError> Errors);
+}
