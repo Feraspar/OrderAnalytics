5e24daa7e5c805e02066e563169911274c7044ae
misha <misha@MIKHAIL>
2026-04-23 22:32:40 +0700

    Добавлены перечисления для региона заказа и статуса

---
+++ OrderAnalytics.Domain/Enums/OrderRegion.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderAnalytics.Domain.Enums
+{
+	/// <summary>
+	/// Регион заказа.
+	/// </summary>
+	public enum OrderRegion
+	{
+		North = 0,
+		South = 1,
+		West = 2,
+		East = 3
+	}
+}
