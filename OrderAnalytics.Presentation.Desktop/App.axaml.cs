6922ee5d95395aec877410b440910fd234b33821
misha <misha@MIKHAIL>
2026-04-23 18:40:07 +0700

    Add project files.

---
+++ OrderAnalytics.Presentation.Desktop/App.axaml.cs
@@ -0,0 +1,32 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using OrderAnalytics.Presentation.Desktop.ViewModels;
+using OrderAnalytics.Presentation.Desktop.Views;
+using System.Linq;
+
+namespace OrderAnalytics.Presentation.Desktop
+{
+	public partial class App : Application
+	{
+		public override void Initialize()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		public override void OnFrameworkInitializationCompleted()
+		{
+			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+			{
+				desktop.MainWindow = new MainWindow
+				{
+					DataContext = new MainWindowViewModel(),
+				};
+			}
+
+			base.OnFrameworkInitializationCompleted();
+		}
+	}
+}
---
+++ OrderAnalytics.Presentation.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OrderAnalytics.Presentation.Desktop.ViewModels
+{
+	public abstract class ViewModelBase : ObservableObject
+	{
+	}
+}
