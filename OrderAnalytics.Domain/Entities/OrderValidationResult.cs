26ac462ed5873c88cd95642fa9d9c0b8d296825f
misha <misha@MIKHAIL>
2026-04-24 18:10:41 +0700

    Реализована сущность для результата валидации

---
+++ OrderAnalytics.Domain/Entities/OrderValidationResult.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderAnalytics.Domain.Entities
+{
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Результат валидации.
+	/// </summary>
+	/// <param name="Orders">Список заказов.</param>
+	/// <param name="Errors">Список ошибок.</param>
+	public record OrderValidationResult(List<Order> Orders, List<ValidationError> Errors);
+}
