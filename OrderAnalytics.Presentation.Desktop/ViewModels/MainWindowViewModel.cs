690e71f37334af6f9e96a001ebfc68c5b96ff218
misha <misha@MIKHAIL>
2026-04-27 18:55:42 +0700

    Реализована верстка и метод перехода на другую страницу

--- OrderAnalytics.Presentation.Desktop/ViewModels/MainWindowViewModel.cs
+++ OrderAnalytics.Presentation.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
 ﻿namespace OrderAnalytics.Presentation.Desktop.ViewModels
 {
+	using CommunityToolkit.Mvvm.Input;
+	using OrderAnalytics.Presentation.Desktop.Enums;
+	using System.Collections.ObjectModel;
+
 	/// <summary>
 	/// Модель представления главного окна.
 	/// </summary>
@@ -11,6 +15,11 @@
 		/// Поле для хранения текущей модели представления в контроле контента.
 		/// </summary>
 		private ViewModelBase? _currentViewModel;
+
+		/// <summary>
+		/// Поле для хранения типа текущей страницы.
+		/// </summary>
+		private AppPage _currentPage;
 
 		#endregion Private Fields
 
@@ -25,6 +34,25 @@
 			set => SetProperty(ref _currentViewModel, value);
 		}
 
+		/// <summary>
+		/// Тип текущей страницы.
+		/// </summary>
+		public AppPage CurrentPage
+		{
+			get => _currentPage;
+			set => SetProperty(ref _currentPage, value);
+		}
+
+		/// <summary>
+		/// Коллекция элементов меню навигации.
+		/// </summary>
+		public ObservableCollection<NavigationItemViewModel> NavigationItems { get; }
+
+		/// <summary>
+		/// Команды для навигации.
+		/// </summary>
+		public IRelayCommand<AppPage> NavigateCommand { get; }
+
 		#endregion Public Properties
 
 		#region Public Constructors
@@ -36,8 +64,41 @@
 		public MainWindowViewModel(ImportViewModel importViewModel)
 		{
 			CurrentViewModel = importViewModel;
+
+			NavigationItems = 
+			[
+				new NavigationItemViewModel("Import", AppPage.ImportPage), 
+				new NavigationItemViewModel("Orders", AppPage.OrdersPage), 
+				new NavigationItemViewModel("Errors", AppPage.ErrorsPage)
+			];
+
+			NavigateCommand = new RelayCommand<AppPage>(Navigate);
+
+			UpdateSelection();
 		}
 
 		#endregion Public Constructors
+
+		/// <summary>
+		/// Навигирует на выбранную страницу.
+		/// </summary>
+		/// <param name="page">Тип выбранной страницы.</param>
+		private void Navigate(AppPage page)
+		{
+			CurrentPage = page;
+
+			UpdateSelection();
+		}
+
+		/// <summary>
+		/// Помечает выбранную страницу.
+		/// </summary>
+		private void UpdateSelection()
+		{
+			foreach (NavigationItemViewModel item in NavigationItems)
+			{
+				item.IsSelected = item.Page == CurrentPage;
+			}
+		}
 	}
 }
