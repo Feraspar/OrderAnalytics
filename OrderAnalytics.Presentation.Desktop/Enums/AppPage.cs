fd69c6221b5bd9ee36f77b7443efeea70f634b5e
misha <misha@MIKHAIL>
2026-04-27 16:56:24 +0700

    Реализация перечисления для типов страниц

---
+++ OrderAnalytics.Presentation.Desktop/Enums/AppPage.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderAnalytics.Presentation.Desktop.Enums
+{
+	/// <summary>
+	/// Перечисление для типа выбранной страницы.
+	/// </summary>
+	public enum AppPage
+	{
+		ImportPage = 0,
+		OrdersPage = 1,
+		ErrorsPage = 2
+	}
+}
