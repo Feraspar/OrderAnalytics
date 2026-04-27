4f91aeae414a250add81639e18d2f0552541c3bc
misha <misha@MIKHAIL>
2026-04-27 21:34:18 +0700

    Реализованы страницы заглушки для проверки работы навигации

---
+++ OrderAnalytics.Presentation.Desktop/ViewModels/ErrorsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderAnalytics.Presentation.Desktop.ViewModels
+{
+	/// <summary>
+	/// Модель представления страницы заказов.
+	/// </summary>
+	public class ErrorsViewModel : ViewModelBase
+	{
+	}
+}
