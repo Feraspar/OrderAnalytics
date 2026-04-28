a16425f9f946fb3f3c7221475fb7ef69ec12b41b
misha <misha@MIKHAIL>
2026-04-28 21:15:53 +0700

    Реализована верстка с таблицей аказов

--- OrderAnalytics.Presentation.Desktop/ViewModels/OrdersViewModel.cs
+++ OrderAnalytics.Presentation.Desktop/ViewModels/OrdersViewModel.cs
@@ -15,6 +15,10 @@
 		/// <inheritdoc <see cref="ImportDataService" />
 		private readonly ImportDataService _importDataService;
 
+		private string _visibleOrdersCount;
+
+		private string _totalOrdersCount;
+
 		#endregion Private Fields
 
 		#region Public Properties
@@ -23,6 +27,18 @@
 		/// Коллекция для хранения заказов.
 		/// </summary>
 		public ObservableCollection<OrderRowViewModel> Orders { get; }
+
+		public string VisibleOrdersCount
+		{
+			get => _visibleOrdersCount;
+			set => SetProperty(ref _visibleOrdersCount, value);
+		}
+
+		public string TotalOrdersCount
+		{
+			get => _totalOrdersCount;
+			set => SetProperty(ref _totalOrdersCount, value);
+		}
 
 		#endregion Public Properties
 
@@ -36,6 +52,10 @@
 		{
 			_importDataService = importDataService;
 			Orders = new ObservableCollection<OrderRowViewModel>(_importDataService.Orders.Select(order => new OrderRowViewModel(order)));
+
+
+			VisibleOrdersCount = Orders.Count.ToString();
+			TotalOrdersCount = Orders.Count.ToString();
 		}
 
 		#endregion Public Constructors
