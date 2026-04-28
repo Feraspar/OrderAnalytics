9756580048ed19e77ded4e68a62163d3d6322015
misha <misha@MIKHAIL>
2026-04-29 01:23:48 +0700

    Добавлено перечисления для значения поля сортировки

---
+++ OrderAnalytics.Presentation.Desktop/Enums/OrdersSortField.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderAnalytics.Presentation.Desktop.Enums
+{
+	/// <summary>
+	/// Перечисление полей для сортировки.
+	/// </summary>
+	public enum OrdersSortField
+	{
+		OrderId = 0,
+		CreatedAt = 1,
+		Manager = 2,
+		Region = 3,
+		Status = 4,
+		Quantity = 5,
+		NetAmount = 6
+	}
+}
